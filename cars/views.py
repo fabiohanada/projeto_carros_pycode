c97809889810671b615def6108a273be3a8eb32a
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2025-07-31 20:56:08 -0300

    filtrando carros

--- cars/views.py
+++ cars/views.py
@@ -4,6 +4,9 @@ from cars.models import Car
 # Create your views here.
 def cars_view(request):
     cars = Car.objects.all()
+    search = request.GET.get('search')
+    if search:
+        cars = Car.objects.filter(model__contains=search)
     
     return render(
         request, 
